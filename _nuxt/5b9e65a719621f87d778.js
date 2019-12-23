8047ef4924d59f3ac8a4660e61003b83422dc045
meer sawood <sawoodrocket@gmail.com>
2019-12-23 22:49:54 +0530

    first commit

---
+++ _nuxt/5b9e65a719621f87d778.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[112],{466:function(n,t,e){"use strict";e.r(t);var l={layout:"dashboard"},o=e(14),component=Object(o.a)(l,function(){var n=this.$createElement;return(this._self._c||n)("div")},[],!1,null,null,null);t.default=component.exports}}]);
